0b13227c58701d97ae074c0f7f50c1b912221d80
HanulGye <kyehl@naver.com>
2018-12-29 14:02:51 +0900

    Initial commit

---
+++ app/src/main/java/tjeit/kr/scrollviewtest/MainActivity.java
@@ -0,0 +1,32 @@
+package tjeit.kr.scrollviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.BaseAdapter;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
